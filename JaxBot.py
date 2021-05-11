1fbea9634b7e2dd1605508a14affcf12aab9c293
Bonesteeler <theo.ebenhoech@gmail.com>
2021-05-10 20:01:11 -0700

    Speak

--- JaxBot.py
+++ JaxBot.py
@@ -4,12 +4,30 @@ import discord
 from dotenv import load_dotenv
 
 load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
+TOKEN = os.getenv('TOKEN')
+GUILD = os.getenv('SERVER')
 
-client = discord.Client()
+intents = discord.Intents.all()
+client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
-    print(f'Woof!')
+    guild = discord.utils.get(client.guilds, name=GUILD)
+    
+    print(
+        f'{client.user} is connected to the following guild:\n'
+        f'{guild.name}(id: {guild.id})\n'
+    )
 
+    members = '\n - '.join([member.name for member in guild.members])
+    print(f'Guild Members:\n - {members}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if "!speak" in message.content:
+        await message.channel.send("Bork")
+    
 client.run(TOKEN)
